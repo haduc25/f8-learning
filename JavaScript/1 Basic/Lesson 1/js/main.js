0f2acb19d7447b6a40b41b04f5b90b718ab8c337
Ha Duc <haducvcvb@gmail.com>
2022-04-20 00:50:25 +0700

    commit js basic / done -> 1, 2. Bien, comments, built-in

--- JavaScript/1 Basic/Lesson 1/js/main.js
+++ JavaScript/1 Basic/Lesson 1/js/main.js
@@ -1,9 +1,84 @@
 // alert('hello guys')
 
 
-//tao bien
-var fullName = 'Ha Minh Duc';
-var age = 21;
+//tao bien (8)
 
-alert(fullName);
-alert(age);
+// var fullName = 'Ha Minh Duc';
+// var age = 21;
+
+// alert(fullName);
+// alert(age);
+
+
+/** (11)
+ * Một số hàm Built-in (là hàm đc xây dựng sẵn)
+ * 1. alert (hien thi tren browser)
+ * 2. console (su dung de debug)
+ * 3. confirm (hien thi tren browser)
+ * 4. prompt (hien thi tren browser)
+ * 5. set timeout (time chạy sau 1 khoảng thời gian đã chỉ định)
+ * 6. set interval (time chạy liên tục deplay trong thời gian đã chỉ định)
+ * 
+ */
+
+//vi du
+/*console
+var fullName = 'Ha Minh Duc';
+console.log(fullName);
+
+console.error(fullName);
+
+console.warn(fullName);
+*/
+
+/*confirm (xac nhan 'ok' & 'cancel')
+var fullName = 'Ha Minh Duc';
+confirm(fullName);
+*/
+
+/*prompt (form input)
+var fullName = 'Ha Minh Duc';
+confirm(fullName);
+*/
+
+/*setTimeout (chay 1 lan sau 1s)
+*cach1: 
+    var fullName = 'Ha Minh Duc';
+    setTimeout(() => {
+        alert(fullName);
+    }, 1000);
+
+*cach2: 
+    var fullName = 'Ha Minh Duc';
+    setTimeout(function() {
+        alert(fullName);
+    },1000)
+
+var fullName = 'Ha Minh Duc';
+confirm(fullName);
+*/
+
+/*setInterval (sau 1s chay 1 lan (lien tuc))
+    var fullName = 'Ha Minh Duc';
+    // sau 1s chay 1 lan (lien tuc)
+    setInterval(() => {
+        console.log(fullName);
+    }, 1000);
+
+
+    var fullName = 'Ha Minh Duc ';
+    // sau 1s chay 1 lan (lien tuc)
+    setInterval(() => {
+        console.log(fullName + Math.random());
+    }, 1000);
+
+
+    *Math.random(): lay ra so random
+*/
+
+
+
+
+
+
+
