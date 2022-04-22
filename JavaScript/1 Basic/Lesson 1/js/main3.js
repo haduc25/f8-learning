700b8c7afe42af4ed0c47a060cce8d44dde815fa
Ha Duc <haducvcvb@gmail.com>
2022-04-22 15:31:26 +0700

    commit js basic / chuong 4 -> 31 -> done & created new folder -> next 32

---
+++ JavaScript/1 Basic/Lesson 1/js/main3.js
@@ -0,0 +1,22 @@
+// chuong 4
+/** 31 - function
+ * - là 1 khối mã
+ * - làm 1 việc cụ thể
+ * Tên hàm - giống các ngôn ngữ khác - có thể có ký tự $
+ * Loại hàm: Built-in & tự định nghĩa
+ * 
+ * 
+ * //ex
+   function showDialog()
+   {
+      var name = 'Ha Duc';
+      var age = 21;
+      alert('Hello my name is ' + name + ' i\'m ' + age + ' years old.' );
+   } 
+
+
+   showDialog();
+*/
+
+
+
