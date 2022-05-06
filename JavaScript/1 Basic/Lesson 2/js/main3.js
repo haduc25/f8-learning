47b0f7d852cb9c80df28077d9a1474abd848572e
Ha Duc <haducvcvb@gmail.com>
2022-05-06 23:58:59 +0700

    chuong 8 -> 63 - 68. Lay ngau nhien phan tu cua mang / next chuong 9 - 69

--- JavaScript/1 Basic/Lesson 2/js/main3.js
+++ JavaScript/1 Basic/Lesson 2/js/main3.js
@@ -237,4 +237,266 @@
 
 /** 63. Object prototype
  * 
+   // Object prototype sử dụng thêm thuộc tính & phương thức vào hàm tạo, từ ngoài vào
+
+      function User(fName, lName, avatar)
+   {
+      //constructor
+      this.fName = fName;
+      this.lName = lName;
+      this.avatar = avatar;
+
+
+      //thêm function
+      this.getName = function()
+      {
+         //nối chuỗi, return -> fullName
+         return `${this.fName} ${this.lName}`;
+      }
+   }
+
+
+
+
+
+
+   //để thêm 1 thuộc tính từ ngoài hàm tạo sử dụng prototype
+
+   //thêm thuộc tính
+   User.prototype.className = 'CNTTK18E';
+
+   //thêm phương thức
+   User.prototype.getClassName = function()
+   {
+      return this.className;
+      //khi user2 gọi function thì this = user2
+   }
+
+   var user = new User('Ha', 'Duc', 'Avt1');
+   var user2 = new User('Ngoc', 'Anh', 'Avt2');
+   
+
+
+   console.log(user.className); //return CNTTK18E
+   console.log(user2.getClassName()); //return CNTTK18E
+
+
+
  */
+
+/** 64. Thêm phương thức getFullName
+Để vượt qua thử thách này, bạn hãy tạo một object constructor Student gồm: firstName, lastName. Sau đó, định nghĩa thêm phương thức là getFullName, 
+phương thức này sẽ trả về tên đầy đủ của sinh viên.
+
+Giữa firstName và lastName cần có 1 khoảng trắng (1 dấu space).
+
+
+
+//code
+   // Làm bài tại đây
+   function Student(firstName, lastName)
+   {
+      this.firstName = firstName;
+      this.lastName = lastName;
+   } 
+
+   Student.prototype.getFullName =function()
+   {
+      return `${this.firstName} ${this.lastName}`;
+   }
+
+
+   // Ví dụ khi sử dụng
+   var student = new Student('Long', 'Bui');
+
+   console.log(student.firstName);  // 'Long'
+   console.log(student.lastName);  // 'Bui'
+   console.log(student.getFullName());  // 'Long Bui'
+ */
+
+/** 65. Đối tượng Date
+ * 
+ * 
+   var date = new Date();
+
+
+   console.log(date); //return Fri May 06 2022 22:52:46 GMT+0700 (Indochina Time)
+   console.log(typeof date); //return object
+
+
+
+   var day = date.getDate(); //day
+   var month = date.getMonth() + 1; //vì month return từ 0-11 nên + 1 
+   var year = date.getFullYear(); //year
+
+   var fullDate = `${day}/${month}/${year}`;
+
+
+   // console.log(day);
+   // console.log(month);
+   // console.log(year); //2022
+   console.log(fullDate); //6/5/2022
+
+
+
+   //
+   // Instance methods
+   // Date.prototype.getDate()
+   // Returns the day of the month (1–31) for the specified date according to local time.
+
+   // Date.prototype.getDay()
+   // Returns the day of the week (0–6) for the specified date according to local time.
+
+   // Date.prototype.getFullYear()
+   // Returns the year (4 digits for 4-digit years) of the specified date according to local time.
+
+   // Date.prototype.getHours()
+   // Returns the hour (0–23) in the specified date according to local time.
+
+   // Date.prototype.getMilliseconds()
+   // Returns the milliseconds (0–999) in the specified date according to local time.
+
+   // Date.prototype.getMinutes()
+   // Returns the minutes (0–59) in the specified date according to local time.
+
+   // Date.prototype.getMonth()
+   // Returns the month (0–11) in the specified date according to local time.
+
+   // Date.prototype.getSeconds()
+   // Returns the seconds (0–59) in the specified date according to local time.
+
+   // Date.prototype.getTime()
+   // Returns the numeric value of the specified date as the number of milliseconds since January 1, 1970, 00:00:00 UTC. (Negative values are returned for prior times.)
+
+   // Date.prototype.getTimezoneOffset()
+   // Returns the time-zone offset in minutes for the current locale.
+
+ */
+
+
+/** 66. Hoàn thiện hàm getNextYear
+   Để vượt qua thử thách này, bạn hãy tạo hàm getNextYear, hàm này sẽ trả về năm kế tiếp. Ví dụ, năm nay là 2022, hàm sẽ trả về 2023 là kiểu số.
+
+   //code
+   function getNextYear()
+   {
+      var date = new Date();
+      var nextYear = date.getFullYear() + 1;
+      return nextYear;
+   }
+ */
+
+
+/** 67.Math object
+ * - Math.PI
+ * - Math.round() //làm tròn trên or dưới (vd: 1,5 -> 2, 1,4 -> 1)
+ * - Math.abs() //giá trị tuyệt đối | |
+ * - Math.ceil() //làm tròn trên, các số thập phân != 0 => làm tròn trên (vd 4.1 => 5)
+ * - Math.floor() //làm tròn dưới, các số thập phân != 0 => làm tròn dưới (vd 4.9 => 4)
+ * - Math.random() //lấy số ngẫu nhiên / default 0 - 1
+ * - Math.min()
+ * - Math.max()
+ 
+
+   //ex - PI
+   console.log(Math.PI); //return 3.141592653589793
+
+   //ex - round()
+   console.log(Math.round(1.5)); //return 2
+   console.log(Math.round(1.49)); //return 1
+
+   //ex - abs()
+   console.log(Math.abs(-100)); //return 100
+
+   //ex - ceil()
+   console.log(Math.ceil(4.1)); //return 5
+   console.log(Math.ceil(4.00001)); //return 5
+
+   //ex - floor()
+   console.log(Math.floor(4.9)); //return 4
+   console.log(Math.floor(4.99999)); //return 4
+
+
+   //ex - random()
+   console.log(Math.random()); //0 - 1
+
+   //vd lấy số ngẫu nhiên từ 0 - 10
+   var numberUnder10 = Math.floor(Math.random() * 10); //dùng Math.floor() / làm tròn dưới
+   var numberUnder100 = Math.floor(Math.random() * 100); //dùng Math.floor() / làm tròn dưới
+   console.log(numberUnder10); //return 0 - 9
+   console.log(numberUnder100); //return 0 - 99
+
+
+   //vd nhận đc phần thưởng ngẫu nhiên 
+   var randUnder5 = Math.floor(Math.random() * 5); //dùng Math.floor() / làm tròn dưới
+
+
+   var reward = [
+      '10 coin',
+      '20 coin',
+      '30 coin',
+      '40 coin',
+      '50 coin'
+   ];
+
+   console.log(reward[randUnder5]); //return 0 - 4
+
+
+   //vd random theo tỷ lệ %
+   var randomPercentUnder100 = Math.floor(Math.random() * 100); //dùng Math.floor() / làm tròn dưới
+
+   //5% thành công
+   if(randomPercentUnder100 < 5){
+      console.log('[random < 5] - Congratulations you has me, your percent is ' + randomPercentUnder100 + '%');
+   }
+
+   //50% thành công
+   if(randomPercentUnder100 < 50){
+      console.log('[random < 50] - Congratulations you has me, your percent is ' + randomPercentUnder100 + '%'); //return Congratulations you has me, your percent is 43
+   }
+
+
+   //ex - min()
+   console.log(Math.min(-10, -5, 0, 5, 10, 20, 25)); //return -10
+
+   //ex - max()
+   console.log(Math.max(-10, -5, 0, 5, 10, 20, 25)); //return 25
+ */
+
+/** 68. Lấy ngẫu nhiên phần tử của mảng
+   Hãy tạo hàm getRandomItem, hàm này nhận 1 tham số là mảng và sẽ trả về ngẫu nhiên 1 phần tử của mảng.
+
+   //code
+
+   function getRandomItem(array)
+   {
+       var rand = Math.floor(Math.random() * array.length);
+       return array[rand];
+   }
+
+
+   cars = [
+      'BMW',
+      'Mazda',
+      'Lexus',
+      'Ford'
+   ];
+
+   console.log(getRandomItem(cars));
+ */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
