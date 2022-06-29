3583df7763424b1951535b14ee5bcec2ea50cdf4
Ha Duc <haducvcvb@gmail.com>
2022-06-29 11:35:19 +0700

    Chuong 14 - 171, 172, 173 / next 174. Promise (chain) / status: 69%, 144/208 course

--- JavaScript/1 Basic/Lesson 3/js/main4.js
+++ JavaScript/1 Basic/Lesson 3/js/main4.js
@@ -156,11 +156,6 @@
     
     // console.log('Dòng này sẽ in ra trước') // Đây là tác vụ đồng bộ (sync)
 
-    */
-
-    
-
-
 
     //170. Promise (pain)
 
@@ -183,10 +178,145 @@
 
 
 
-    
-    
 
 
+    
+    //171. Promise (concept)
+    // - Lý thuyết, cách hoạt động
+
+    //Tóm tắt
+    // 1. Tạo Promise() / new Promise()
+    // 2. Tạo ra Executor
+
+    // Exception
+    // - Nếu trong 'Executor' k gọi đến 'resolve' hoặc 'reject' sẽ bị treo -> Memory Leak (Rò rỉ bộ nhớ)
+
+    // - promise trả lại 3 phương thức: .then(), .catch(), .finally() 
+
+    //Promise có 3 trạng thái
+    // 1. Pending: Chưa giải quyết / đang chờ trả về 'resolve' hay 'reject' / -> Memory Leak
+    // 2. Fulfilled: Hoàn Thành / resolve()
+    // 3. Rejected: Thất bại, Phế phẩm / reject()
+
+
+
+    var promise = new Promise(
+
+        //Nhận vào 1 function 
+        //Executor / sẽ đc thực thi khi gọi đến `promise`
+        function(resolve, reject) { //nhận vào 2 tham số: resolve, reject
+            //Logic
+            //Resolve(): Thành công, Giải quyết
+            //Reject(): Thất bại, từ chối
+
+            //resolve -> .then()
+            // resolve(); //return Successfully! & Done!
+
+            //reject -> .catch()
+            // reject(); //return Failure! & Done!
+
+
+            //ex - Fake call API / Trả dữ liệu
+            // //resolve()
+            // resolve([
+            //     {
+            //         id: 1,
+            //         name: 'Jett'
+            //     }
+            // ]);
+
+            //reject()
+            reject('Đã có lỗi xảy ra, vui lòng thử lại.');
+
+
+        }        
+    );
+
+
+    // //exec promise
+    // promise
+    //     .then(function(){
+    //         //sẽ chạy khi trong 'Executor' gọi đến 'resolve'    
+    //         console.log('Successfully!');
+    //     })
+
+    //     .catch(function(){
+    //         //sẽ chạy khi trong 'Executor' gọi đến 'reject'
+    //         console.log('Failure!'); 
+    //         //Failure!: Thất bại
+    //     })
+
+    //     .finally(function(){
+    //         //sẽ chạy khi trong 'Executor' gọi đến 'resolve' hoặc 'reject'
+    //         console.log('Done!');
+    //     });
+
+    // *Fact: Có thể bỏ .finally() nếu k dùng
+
+
+
+    //ex - Fake call API / Trả dữ liệu
+    promise
+        .then((course) => {
+            //print ra course
+            console.log(course); //return [{…}]
+        })
+
+        .catch((error) => {
+            console.log(error); //return Đã có lỗi xảy ra, vui lòng thử lại.
+
+            // *Fact: Nếu bỏ .catch() sẽ xuất hiện lỗi 'Uncaught (in promise) Đã có lỗi xảy ra, vui lòng thử lại.', -> thêm lại .cacth()
+        })
+
+        .finally(() => {
+            console.log('Done!');
+            // *Fact: Có thể bỏ .finally() nếu k dùng
+        });
+
+
+    // - Cách trả lời phỏng vấn khi hỏi về 'Promise'.
+    // 1. Khái niệm 'Promise'?
+    // + Promise sinh ra để giúp xử lý những thao tác bất đồng bộ (Async)
+    // + Trước khi có 'Promise' thường là sử dụng 'Callback' / 'Callback' thường xảy ra vấn đề là 'Callback hell' sẽ bị sâu code -> khó nhìn, khó hiểu
+    // + Vì vậy 'Promise' được sinh ra từ JavaSript ES6 -> 'Promise' sử dụng để khắc phục tình trạng 'Callback hell' -> dễ đọc, dễ hiểu hơn
+    
+    // Để tạo ra 1 'Promise'
+    // 1. Sử dụng từ khóa 'new Promise()'
+    // 2. Ở trong Constructor truyền vào 1 'Executor function', trong 'Executor function'nhận vào 2 tham số khi 'Executor' thực thi là: 'resolve' & 'reject'
+    // - resolve() sẽ được gọi khi thao tác Logic xử lý thành công    
+    // - reject() sẽ được gọi khi thao tác Logic xử lý thất bại
+    // - Khi sử dụng 'Promise' -> đối tượng 'promise' được tạo ra và ta sẽ sử dụng qua các phương thức: .then(), .catch() 
+    // .then(), .catch() đều nhận 'Callback function' 
+    // .then() sẽ được thực thi khi mà 'Promise' được 'resolve' 
+    // .catch() sẽ được thực thi khi mà 'Promise' được 'reject'
+
+    // 'Promise' sẽ được xử lý trong những trường hợp bất đồng bộ (Async)
+
+
+    // 172. Ôn lại kiến thức về Promise #1
+    // Promise có bao nhiêu trạng thái?
+    // - Promise có 3 trạng thái:
+    //  + Pending
+    //  + Fulfilled
+    //  + Rejected
+    
+    // 173. Ôn lại kiến thức về Promise #2
+    // 3 trạng thái của Promise là gì?
+    // - Promise có 3 trạng thái:
+    //  + Pending
+    //  + Fulfilled
+    //  + Rejected
+
+
+
+
+
+
+    */
+
+
+
+    //174. Promise (chain)
 
 
 
