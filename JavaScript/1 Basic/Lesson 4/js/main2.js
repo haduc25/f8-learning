cdfdb8c34b3afe90c73f5e7983b453e228f36a65
Ha Duc <haducvcvb@gmail.com>
2022-07-24 18:52:51 +0700

    Chuong 17 - 198 Coding / 26m57s / status: 83%, 171/206 course

--- JavaScript/1 Basic/Lesson 4/js/main2.js
+++ JavaScript/1 Basic/Lesson 4/js/main2.js
@@ -2,4 +2,151 @@
     198. Xử lý báo lỗi cơ bản
 
 
+    
 */
+ 
+    // Tạo function
+    // Đối tượng `Validator`
+    function Validator(options){
+        console.log(options); //return {form: 'form-1', rules: Array(2)}
+        console.log(options.form); //return '#form-1'
+        console.log(options.rules);// return (2) [undefined, undefined]
+
+        // Lấy form ELement
+        var formElement = document.querySelector(options.form);
+
+        if(formElement){
+            // console.log(formElement);
+
+            // vì options.rules = là array => duyệt qua array
+            options.rules.forEach((rule) => {
+                // console.log(rule); //return  {selector: '#fullname', test: ƒ} {selector: '#email', test: ƒ}
+
+                // lấy ra vale từ rules
+                // console.log(rule.selector); //return #fullname #email
+
+                // lấy ra thẻ input / đi từ 'formElement'
+                var inputElement = formElement.querySelector(rule.selector);
+
+                // console.log(inputElement); 
+                // <input id="fullname" name="fullname" type="text" placeholder="VD: Sơn Đặng" class="form-control">
+                // <input id="email" name="email" type="text" placeholder="VD: email@domain.com" class="form-control">
+                
+                if(inputElement){
+                    // Event on blur
+                    inputElement.onblur = () => {
+                        // console.log('blur: '+ rule.selector);
+                        // return
+                        // blur: #fullname
+                        // blur: #email
+
+                        console.log(inputElement.value); //lấy ra value của thẻ 'input'
+                        console.log(rule); //return {selector: '#fullname', test: ƒ} / khi blur input #fullname
+
+                        // value: inputElement.value
+                        // test function: rule.test
+
+
+                        // run function / check error
+                        var errorMessage = rule.test(inputElement.value);
+
+                        // console.log(errorMessage); //return Vui lòng nhập trường này / undefined
+
+                        // nếu có lỗi
+                        if(errorMessage){
+                            // 
+                        }
+                    }
+                }
+
+            });
+        }
+    }
+
+
+
+    // Định nghĩa Rules
+    // Tạo ra các phương thức gồm các rules 
+
+    // * Nguyên tắc của các rules
+    // 1. Khi có lỗi => trả ra message lỗi
+    // 2. Khi hợp lệ => trả về 'undefined'
+
+
+    // isRequired
+    Validator.isRequired = function(selector){ //nhận vafp selector
+        // return selector; //(2) ['#fullname', '#email']  
+
+        return {
+            selector: selector,
+            test: function(value){
+                // Kiểm tra xem users đã nhập hay chưa?
+
+                // Logic: Nếu users có nhập 'value' (value = true) => return undefined / k có value => return 'Vui lòng nhập trường này'
+                // dùng trim(): bỏ space đầu, cuối
+                return value.trim() ? undefined : 'Vui lòng nhập trường này'; 
+            }
+        } // (2) [{…}, {…}]
+    }
+
+    // isEmail
+    Validator.isEmail = function(selector){ //nhận vafp selector
+        // return selector; //(2) ['#fullname', '#email']  
+        
+        return {
+            selector: selector,
+            test: function(){
+                
+            }
+        } // (2) [{…}, {…}]
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // Call function
+    // Mong muốn sử dụng sau khi hoàn thành thư viện
+    Validator({
+        form: '#form-1', //truyền vào form cần validate
+        rules: [
+            Validator.isRequired('#fullname'), //truyền vào CSS Selector của input cần rule 'isRequired()'
+            Validator.isEmail('#email'), //truyền vào CSS Selector của input cần rule 'isEmail()'
+        ]
+    });
+
+
+
+
+
+
+
+
+
+    // Youtube: 24/07/2022
+    // https://youtu.be/O2PpHVKGsIg
+    // https://youtu.be/Zu3XREOy6_M
