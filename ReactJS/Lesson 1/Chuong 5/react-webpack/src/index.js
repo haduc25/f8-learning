6170a5a5e75663825d977079d841d3cb71cd2346
Ha Duc <haducvcvb@gmail.com>
2022-10-05 11:16:12 +0700

    C5 - 41, 42 / next 43. Tao du an voi create-react-app / 34%, 41/119 course

--- ReactJS/Lesson 1/Chuong 5/react-webpack/src/index.js
+++ ReactJS/Lesson 1/Chuong 5/react-webpack/src/index.js
@@ -1,5 +1,5 @@
 import React from 'react' // nạp thư viện react
-import ReactDOM from 'react-dom' // nạp thư viện react-dom
+import ReactDOM from 'react-dom/client' // nạp thư viện react-dom
 
 // Tạo component App
 function App() {
@@ -13,6 +13,12 @@ function App() {
     )
 }
 
+// React@17
 // Render component App vào #root element
-ReactDOM.render(<App />, document.getElementById('root'))
+// ReactDOM.render(<App />, document.getElementById('root'))
+
+
+// React@18
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App/>)
 
