6e5e48a5068fe7dd2176a17f496ccb342c4a17f8
Ha Duc <haducvcvb@gmail.com>
2022-10-06 19:03:45 +0700

    C6 - 48, 49 / next 50. Two-way binding trong React? / 40%, 48/119 course

--- ReactJS/Lesson 1/Chuong 5/reactjs/tiktok/src/App.js
+++ ReactJS/Lesson 1/Chuong 5/reactjs/tiktok/src/App.js
@@ -1,25 +1,160 @@
-import logo from './logo.svg';
-import './App.css';
+/** Chương 6
+ * 48 -  useState hook
+ * 
+ * 
+ * 
+ */
+
+// // Ex - Ứng dụng đếm số khi clicked vào button / bắt đầu từ số 1
+
+// import { useState } from 'react'
+
+// function App() {
+
+//   // create State
+//   const [counter, setCounter] = useState(1)
+
+//   //fucntion handleIncrease
+//   const handleIncrease = () =>{
+//       // gọi hàm setCounter()
+//       setCounter(counter + 1)
+//   }
+
+//   return (
+//     <div className="App" style={{ textAlign: 'center' }}>
+//         {/* render */}
+//         <h1>{counter}</h1>
+//         <button onClick={handleIncrease}>Increase</button>
+//     </div>
+//   );
+// }
+
+
+
+
+
+// // Ex - Set state với callback
+// import { useState } from 'react'
+
+// function App() {
+//   const [counter, setCounter] = useState(1)
+
+//   const handleIncrease = () =>{
+//     // callback return curentValue / giá tri hiện tại / 1
+//     setCounter(prevState => prevState + 1) //2
+//     setCounter(prevState => prevState + 1) //3
+//     setCounter(prevState => prevState + 1) //4
+//   }
+
+//   console.log('re-render'); //chạy 1 lần 
+
+//   return (
+//     <div className="App" style={{ textAlign: 'center' }}>
+//         {/* render */}
+//         <h1>{counter}</h1>
+//         <button onClick={handleIncrease}>Increase</button>
+//     </div>
+//   );
+// }
+
+
+
+
+
+// // Ex - Initial state với callback
+// import { useState } from 'react'
+
+// const orders = [100, 200, 300, 400]
+
+// function App() {
+
+  
+//   const [counter, setCounter] = useState(() => {
+//     const total = orders.reduce((total, cur) => total + cur)
+//     console.log(total)
+
+//     return total
+//   })
+
+//   const handleIncrease = () =>{
+//     setCounter(prevState => prevState + 1)
+//   }
+
+
+//   return (
+//     <div className="App" style={{ textAlign: 'center' }}>
+//         <h1>{counter}</h1>
+//         <button onClick={handleIncrease}>Increase</button>
+//     </div>
+//   );
+// }
+
+
+
+
+
+// Ex - Set state là thay thế state bằng giá trị mới
+import { useState } from 'react'
 
 function App() {
+
+  
+  const [info, setInfo] = useState({
+    name: 'Ha Minh Duc',
+    age: 22,
+    address: 'Bac Kan, VN'
+  })
+
+  const handleUpdate = () =>{
+    // // khi clicked => thay thế obj
+    // setInfo({
+    //   facebook: 'haduc.25.09'
+    // })
+
+    // // nếu muốn thêm giá trị vào obj
+    // // case 1: dùng spread giải obj cũ | dùng khi bài toán đơn giản
+    setInfo({
+      ...info,
+      facebook: 'haduc.25.09'
+    })
+
+    // // case 2: dùng callback + spread
+    // setInfo(prev => ({
+    //   ...prev,  
+    //   facebook: 'haduc.25.09'
+    // }))
+
+    // // case 3: dùng callback + spread + logic => return | dùng khi cần xử lý logic
+    // setInfo(prev => {
+    //   // logic...
+
+    //   return {
+    //     ...prev,  
+    //     facebook: 'haduc.25.09'
+    //   }
+    // })
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={{ textAlign: 'center' }}>
+      {/* Render Object => dùng 'JSON.stringify(urObj)' */}
+        <h1>{JSON.stringify(info)}</h1>
+        <button onClick={handleUpdate}>Update</button>
     </div>
   );
 }
+
+
+
+
+
+
+
+
+
+
+
+
 
 export default App;
