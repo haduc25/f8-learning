06591c16836e77cbd01d5bdd8d27ec91d5439112
Ha Duc <haducvcvb@gmail.com>
2022-10-10 18:05:02 +0700

    C6 - 59. 7m52s / next 60. useEffect with preview avatar / 49%, 59/119 course

--- ReactJS/Lesson 1/Chuong 5/reactjs/tiktok/src/60/Content.js
+++ ReactJS/Lesson 1/Chuong 5/reactjs/tiktok/src/60/Content.js
@@ -16,27 +16,84 @@
  */
 
 
-//===> Ex - 3. 'Cleanup' function luôn dược gọi trước khi 'callback'  được gọi (trừ lần mounted)
+// //===> Ex - 3. 'Cleanup' function luôn dược gọi trước khi 'callback'  được gọi (trừ lần mounted)
+
+// import { useEffect, useState } from 'react'
+
+
+// function Content(){
+//     const [count, setCount] = useState(1)
+
+//     // count = deps => count tăng callback đc gọi
+//     useEffect(() =>{
+//         console.log(`Mounted or Re-render lần ${count}`);
+
+//         // Clear func
+//         return () => console.log(`Cleanup lần ${count}`);
+//     }, [count])
+
+
+//     return (
+//         <div>
+//             <h2>{count}</h2>
+//             <button onClick={() => setCount(count + 1)}> Click me ^^</button>
+//         </div>
+//     )
+
+// }
+
+
+
+
+/** Tổng kết về Cleanup
+ * - Trước khi gọi callback mới => Cleanup lần trước đó
+ */
+
+
+
+
+
+// //===> Ex - Ứng dụng về Cleanup - tạo ứng dụng cho phép chọn ảnh avatar -> preview image
 
 import { useEffect, useState } from 'react'
 
 
 function Content(){
-    const [count, setCount] = useState(1)
+    // dat avatar
+    const [avatar, setAvatar] = useState()
 
-    // count = deps => count tăng callback đc gọi
-    useEffect(() =>{
-        console.log(`Mounted or Re-render`);
 
-        // Clear func
-        return () => console.log(`Cleanup`);
-    }, [count])
+    // handlePreviewAvatar
+    const handlePreviewAvatar = (e) =>{
+        // lấy ra file
+        const file = e.target.files[0] //ở đây xử lý 1 image => lấy pt thứ 0, ảnh đầu tiên
+        
+        // tạo ra url ảo/tạm => sử dụng 'URL.createObjectURL()'
+        console.log(file); //return File {name: 'cute-1.jpg', lastModified: 1665398781295, lastModifiedDate: Mon Oct 10 2022 17:46:21 GMT+0700 (Indochina Time), webkitRelativePath: '', size: 197508, …}
+        console.log(URL.createObjectURL(file)); //return blob:http://localhost:3000/8b48c78c-a1c0-4fb5-aaa6-2a60c4684109
+
+        // Vì file là obj => thêm property mới -> preview / lưu url của avatar
+        file.preview = URL.createObjectURL(file)
+
+        // Set lại avatar
+        setAvatar(file)         
+        
+        
+    }
 
 
     return (
         <div>
-            <h2>{count}</h2>
-            <button onClick={() => setCount(count + 1)}> Click me ^^</button>
+            <input 
+                type='file'
+                onChange={handlePreviewAvatar}
+            />
+            <br/><br/><br/>
+            {/* Nếu có avatar => hiển thị/return img */}
+            {avatar && (
+                //Trên đã set property 'preview' => ở đây có thể dùng
+                <img src={avatar.preview} alt='' width='60%' />
+            )}
         </div>
     )
 
