fdce70ca8ecfd465048dad270ec06848ad0c6018
Ha Duc <haducvcvb@gmail.com>
2022-10-13 10:38:15 +0700

    C6 - / sưa lai bai 68 viet nham 67/ done 68 / next 69, On tap useCallback / 56%, 67/119 course

--- ReactJS/Lesson 1/Chuong 5/reactjs/tiktok/src/67/Content.js
+++ ReactJS/Lesson 1/Chuong 5/reactjs/tiktok/src/67/Content.js
@@ -7,6 +7,16 @@ import { memo } from 'react'
 function Content({ onIncrease }) {
     console.log('re-render'); //k dc sử dụng nhưng vẫn bị re-render
 
+
+    /** Thêm thẻ grab 
+     *  <React.Fragment></React.Fragment>
+     *  Shorthand: <></>
+    */
+
+    /** Logic:
+     * Khi click vào btn Click me!
+     * + gọi ngược ra ngoài -> gọi tới 'handleIncrease' 
+    */
     return (
         <>
         <h2>My reflection is here. Do me a favour, cariño. Kill her. |</h2>
