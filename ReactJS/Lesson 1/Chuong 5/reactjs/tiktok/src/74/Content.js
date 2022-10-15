7ec453382cf1e09b4fc41c41ea403b1e13acf3e6
Ha Duc <haducvcvb@gmail.com>
2022-10-15 11:48:39 +0700

    C6 - 74. Coding 5m00s / next 75. useReducer recap / 61%, 73/119 course

--- ReactJS/Lesson 1/Chuong 5/reactjs/tiktok/src/74/Content.js
+++ ReactJS/Lesson 1/Chuong 5/reactjs/tiktok/src/74/Content.js
@@ -1,18 +1,68 @@
  //======================> 74 - Todo App with useReducer hook <======================//
 
 /** => useReducer()
- *      1. Init state: 0
- *      2. Actions: Up (state + 1) / Down (state - 1)
+ *      1. Init state
+ *      2. Actions
  *      3. Tạo ra 'Reducer'
  *      4. Dispatch //Kích hoạt 1 action
 */
 
+/** => Phân tích bài toán
+ *      1. Init state: có 2 state
+ *          - state thể hiện input khi người dùng nhập vào / job => string
+ *          - state thể hiện danh sách khi nhập xong dữ liệu / jobs => array
+ *          *Lưu ý: để dùng nhiều state => cho vào obj
+ *      
+ *      2. Actions
+ *          - Gồm 3 actions: SET_JOB, ADD_JOB, DELETE_JOB
+ * 
+ *      3. Tạo ra 'Reducer'
+ *          - Theo nguyên tắc là hàm(func) nhận vào state, action
+ * 
+ *      4. Dispatch
+ *          - import { useReducer } from 'react'
+ * 
+ * 
+*/
+
+
+// import
+import { useReducer } from 'react'
+
+
+// 1. Init state
+const initState = {
+    job: '',
+    jobs: []
+}
+
+// 2. Actions
+const SET_JOB = 'set_job' //đi set lại (job: '') ở trên
+const ADD_JOB = 'add_job'
+const DELETE_JOB = 'delete_job'
+
+// 3. Tạo ra 'Reducer'
+const reducer = (state, action) => {
+
+}
+
+
+// 4. Dispatch
+
+
 
 // ===> Ex - Sử dụng useReducer() / App todoList / Cho trước App todoList chưa xử lý Logic
 
-import { useReducer } from 'react'
 
 function Content() {
+    // tạo state
+    const [state, dispatch] = useReducer(reducer, initState)
+
+    console.log(state); //return {job: '', jobs: Array(0)}
+
+    // dùng destructuring lấy dl ra
+    const { job, jobs } = state
+
     return (
         <>
             <h3>TodoList</h3>
