27bba1c88d3556a3c749070ed3355747c6912cf1
Ha Duc <haducvcvb@gmail.com>
2022-10-18 09:28:36 +0700

    C6 - 78. Coding 19m23s | added logger.js & editing Provider.js, index.html / next 79 / 64%, 77/119 course

--- ReactJS/Lesson 1/Chuong 5/reactjs/tiktok/src/78/store/Provider.js
+++ ReactJS/Lesson 1/Chuong 5/reactjs/tiktok/src/78/store/Provider.js
@@ -1,11 +1,12 @@
 import { useReducer } from 'react'
 import Context from './Context'
 import reducer, { initState } from './reducer'
+import logger from './logger'
 
 // tao provider
 function Provider({ children }){
     // tao state
-    const [state, dispatch] = useReducer(reducer, initState)
+    const [state, dispatch] = useReducer(logger(reducer), initState)
 
     return(
         //value: truyền qua array cho tất cả các Component bên dưới đều có thể lấy được state & dispatch
