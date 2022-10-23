80e30f43f5b36261063d3c5699ce343103d55a7a
Ha Duc <haducvcvb@gmail.com>
2022-10-23 12:22:58 +0700

    C9 - 93 Done | 94. Cau hinh su dung CSS/SASS / 78%, 93/119 course

--- ReactJS/Lesson 2/Chuong 9/reactjs/tiktok-ui/src/App.js
+++ ReactJS/Lesson 2/Chuong 9/reactjs/tiktok-ui/src/App.js
@@ -2,32 +2,27 @@
 
 import logo from './logo.svg';
 import './App.css';
-import Main from './92/Main'
+import Main from './92/Main';
 
 function App() {
-  return (
-    <div className="App" style={{ textAlign: 'center' }}>
-      <Main />
+    return (
+        <div className="App" style={{ textAlign: 'center' }}>
+            <Main />
 
-      <br/><br/>
+            <br />
+            <br />
 
-
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <p>
+                    Edit <code>src/App.js</code> and save to reload.
+                </p>
+                <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+                    Learn React
+                </a>
+            </header>
+        </div>
+    );
 }
 
 export default App;
