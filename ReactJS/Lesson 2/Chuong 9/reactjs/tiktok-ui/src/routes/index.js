b6c233db4f153ee769792c4b7fd0e22a3c6da169
Ha Duc <haducvcvb@gmail.com>
2022-10-23 16:12:26 +0700

    C9 - 95. Coding | 16m15s / next C10 / 78%, 94/119 course

--- ReactJS/Lesson 2/Chuong 9/reactjs/tiktok-ui/src/routes/index.js
+++ ReactJS/Lesson 2/Chuong 9/reactjs/tiktok-ui/src/routes/index.js
@@ -1,12 +1,14 @@
 // 5. Đưa cấu hình routes ra ngoài
 import Home from '~/95/pages/Home';
 import Following from '~/95/pages/Following';
+import Profile from '~/95/pages/Profile';
 
 // Public Routes
 const publicRoutes = [
     //Dùng cho nhưng router k cần đăng nhập vẫn xem được
     { path: '/', component: Home },
     { path: '/following', component: Following },
+    { path: '/profile', component: Profile },
 ];
 
 // Private Routes
