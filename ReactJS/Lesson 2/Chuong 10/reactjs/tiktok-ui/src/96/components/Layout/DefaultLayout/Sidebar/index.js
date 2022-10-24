dd7d208f4b17db39366ad88856d401abaaf1031c
Ha Duc <haducvcvb@gmail.com>
2022-10-24 11:38:16 +0700

    C10 - 96. Coding | 20m27s - Header, Sidebar / 79%, 95/119 course

--- ReactJS/Lesson 2/Chuong 10/reactjs/tiktok-ui/src/96/components/Layout/DefaultLayout/Sidebar/index.js
+++ ReactJS/Lesson 2/Chuong 10/reactjs/tiktok-ui/src/96/components/Layout/DefaultLayout/Sidebar/index.js
@@ -1,5 +1,22 @@
+import classNames from 'classnames/bind';
+import styles from './Sidebar.module.scss';
+
+const cx = classNames.bind(styles);
+
 function Sidebar() {
-    return <h2>Sidebar</h2>;
+    /** <aside> - Definition and Usage
+     *  - The <aside> tag defines some content aside from the content it is placed in.
+     *  - The aside content should be indirectly related to the surrounding content.
+     *  - Tip: The <aside> content is often placed as a sidebar in a document.
+     *  - Note: The <aside> element does not render as anything special in a browser. However, you can use CSS to style the <aside> element.
+     *  - Link: https://www.w3schools.com/tags/tag_aside.asp
+     */
+
+    return (
+        <aside className={cx('wrapper')}>
+            <h2>Sidebar</h2>
+        </aside>
+    );
 }
 
 export default Sidebar;
