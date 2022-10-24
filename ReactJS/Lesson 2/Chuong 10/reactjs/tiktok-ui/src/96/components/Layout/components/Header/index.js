48e66a5665b99c325110242126915c5365075b15
Ha Duc <haducvcvb@gmail.com>
2022-10-24 11:20:21 +0700

    C10 - 96. Coding | 15m05s / 79%, 95/119 course

--- ReactJS/Lesson 2/Chuong 10/reactjs/tiktok-ui/src/96/components/Layout/components/Header/index.js
+++ ReactJS/Lesson 2/Chuong 10/reactjs/tiktok-ui/src/96/components/Layout/components/Header/index.js
@@ -1,5 +1,27 @@
+import classNames from 'classnames/bind';
+
+import styles from './Header.module.scss';
+
+// tạo biến cx
+const cx = classNames.bind(styles); //dùng method bind()
+
+/** bind()
+ * - bind obj styles
+ * - return function vào cx
+ * => Có thể viết ddc classname dưới dạng dấu - | 'ha-duc'
+ */
+
 function Header() {
-    return <h2>Header</h2>;
+    return (
+        <header className={cx('wrapper')}>
+            <div className={cx('inner')}>
+                {/* Logo */}
+                {/* Search */}
+                {/* ... */}
+                Header
+            </div>
+        </header>
+    );
 }
 
 export default Header;
