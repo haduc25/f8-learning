16208aef764b8b9acaca6d00cf7d1e4febde9843
Ha Duc <haducvcvb@gmail.com>
2022-10-25 12:17:52 +0700

    C10 - 98. Added tippy/headless, interactive, visible, keyword | 12m18s / 81%, 97/119 course

--- ReactJS/Lesson 2/Chuong 10/reactjs/tiktok-ui/src/96/components/Layout/components/Header/index.js
+++ ReactJS/Lesson 2/Chuong 10/reactjs/tiktok-ui/src/96/components/Layout/components/Header/index.js
@@ -1,6 +1,7 @@
+import { useEffect, useState } from 'react';
 import classNames from 'classnames/bind';
-import Tippy from '@tippyjs/react';
-import 'tippy.js/dist/tippy.css'; // optional
+import Tippy from '@tippyjs/react/headless';
+// import 'tippy.js/dist/tippy.css'; // optional
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCircleXmark, faSpinner, faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
@@ -18,6 +19,17 @@ const cx = classNames.bind(styles); //dùng method bind()
  */
 
 function Header() {
+    // Search Result, default = array
+    const [searchResult, setSearchResult] = useState([]);
+
+    // Fake API
+    useEffect(() => {
+        // sau 3s => set lai result
+        setTimeout(() => {
+            setSearchResult([1, 2, 3]);
+        }, 3000);
+    });
+
     // console.log(images.logo.default); //return "/static/media/logo.e8af434aa91f33b2c32bb97d5fee0a28.svg"
 
     return (
@@ -27,21 +39,31 @@ function Header() {
                 <img src={images.logo} alt="Tiktok" />
 
                 {/* Search */}
-                <div className={cx('search')}>
-                    <input placeholder="Search accounts and videos" spellCheck={false} />
-                    {/* Clear */}
-                    <button className={cx('clear')}>
-                        <FontAwesomeIcon icon={faCircleXmark} />
-                    </button>
-                    {/* Loading */}
-                    <FontAwesomeIcon className={cx('loading')} icon={faSpinner} />
-                    {/* Search */}
-                    <Tippy content="Tìm kiếm" placement="right">
+                <Tippy
+                    interactive //interactive : it can be hovered over or clicked without hiding.
+                    // Logic(visible): Nếu có kq => return true => show result
+                    visible={searchResult.length > 0}
+                    render={(attrs) => (
+                        <div className={cx('search-result')} tabIndex={-1}>
+                            ket qua
+                        </div>
+                    )}
+                >
+                    <div className={cx('search')}>
+                        <input placeholder="Search accounts and videos" spellCheck={false} />
+                        {/* Clear */}
+                        <button className={cx('clear')}>
+                            <FontAwesomeIcon icon={faCircleXmark} />
+                        </button>
+                        {/* Loading */}
+                        <FontAwesomeIcon className={cx('loading')} icon={faSpinner} />
+                        {/* Search Button*/}
+
                         <button className={cx('search-btn')}>
                             <FontAwesomeIcon icon={faMagnifyingGlass} />
                         </button>
-                    </Tippy>
-                </div>
+                    </div>
+                </Tippy>
 
                 {/* Actions */}
                 <div className={cx('actions')}>Action here</div>
