921c55a1773ee1c6a0bfae277adf969fad344e57
Ha Duc <haducvcvb@gmail.com>
2022-11-07 01:02:53 +0700

    C10 - 115. 34m10s | MenuItem, css: Sidebar>wrapper, Menu>menu-item>active | 95%, 114/119

--- ReactJS/Lesson 2/Chuong 10/reactjs/tiktok-ui/src/layouts/components/Sidebar/Menu/MenuItem.js
+++ ReactJS/Lesson 2/Chuong 10/reactjs/tiktok-ui/src/layouts/components/Sidebar/Menu/MenuItem.js
@@ -8,7 +8,15 @@ const cx = classNames.bind(styles);
 
 function MenuItem({ title, to, icon }) {
     return (
-        <NavLink to={to} className={cx('menu-item')}>
+        /** className={cx('menu-item') => className={(nav) => cx('menu-item')
+         * Chuyển từ truyền chuỗi sang truyền function
+         * return lại chính className (đã đc convert) / menu-item'\
+         * Vì là NavLink return ra callback là navData => tạo ra 'nav' nhận dl từ 'navData'
+         * trong 'nav' => có 1 key là 'isActive'
+         * Logic: nav.isActive là của 'NavLink' => khi đc active => nav.isActive = true
+         * Thêm 'end' tự xóa 'active'
+         */
+        <NavLink to={to} className={(nav) => cx('menu-item', { active: nav.isActive })} end>
             {icon}
             <span className={cx('title')}>{title}</span>
         </NavLink>
