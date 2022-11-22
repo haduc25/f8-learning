fab3a123873602a234b4fe37e072c7e3d9b3e322
Ha Duc <haducvcvb@gmail.com>
2022-11-22 11:29:44 +0700

    NodeJS - Fixing #3

--- NodeJS & ExpressJS/blog/index.js
+++ NodeJS & ExpressJS/blog/index.js
@@ -1,6 +1,10 @@
 const express = require('express')
+var morgan = require('morgan')
+
 const app = express()
 const port = 3000
+
+app.use(morgan('combined')) //type combined / Predefined Formats
 
 app.get('/', (req, res) => {
   res.send('Hello World! <3 | ^^ | haduc25')
