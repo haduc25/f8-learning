b02694a5b09598a7dc6ccfdd5e898f381d89ce4a
Ha Duc <haducvcvb@gmail.com>
2022-11-23 12:28:09 +0700

    19. Mô hình MVC, 20. [MVC] Routes & Controllers / created app/controllers & routes folder #1

--- NodeJS & ExpressJS/blog/src/index.js
+++ NodeJS & ExpressJS/blog/src/index.js
@@ -6,6 +6,8 @@ const path = require('path');
 const app = express();
 const port = 3000;
 
+const route = require('./routes');
+
 app.use(express.static(path.join(__dirname, 'public'))); //cho phép truy cập file tĩnh / public
 // console.log('PATH IMAGE: ', path.join(__dirname, 'public/img'));
 
@@ -26,36 +28,8 @@ app.set('views', path.join(__dirname, 'resources/views'));
 // console.log('PATH: ', __dirname); //PATH:  D:\Coding\F8_Learning\NodeJS & ExpressJS\blog\src
 // console.log('PATH: ', path.join(__dirname, 'resources/views')); //PATH:  D:\Coding\F8_Learning\NodeJS & ExpressJS\blog\src\resources\views
 
-app.get('/', (req, res) => {
-    // res.send(`
-    //   <h2>Hello World! - haduc25</h2>
-    // `);
-
-    res.render('home');
-});
-
-app.get('/news', (req, res) => {
-    console.log(req.query);
-    res.render('news');
-});
-
-// app.post('/news', (req, res) => {
-//     res.render('news');
-// });
-
-app.get('/search', (req, res) => {
-    // console.log('res/query: ', req.query); //RES => QUERY:  { q: 'haduc25', ref: 'haduc2509' }
-    console.log('res/query/q: ', req.query.q); //haduc25
-    console.log('res/query/q: ', req.query.ref); //haduc2509
-
-    res.render('search');
-});
-
-app.post('/search', (req, res) => {
-    // Với POST thì nhận data qua 'req.body'
-    console.log(req.body); //{ q: 'toiyeubanvclmabandeobienchanvcl', 'full-name': 'bmh <3', age: '18', gender: 'famale'}
-    res.send('SEARCH ĐÃ NHẬN ĐC DATA / METHOD POST');
-});
+// Routes init / truyền 'app' qua route
+route(app);
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
---
+++ NodeJS & ExpressJS/blog/src/routes/index.js
@@ -0,0 +1,34 @@
+function route(app) {
+    app.get('/', (req, res) => {
+        // res.send(`
+        //   <h2>Hello World! - haduc25</h2>
+        // `);
+
+        res.render('home');
+    });
+
+    // app.get('/news', (req, res) => {
+    //     console.log(req.query);
+    //     res.render('news');
+    // });
+
+    // app.post('/news', (req, res) => {
+    //     res.render('news');
+    // });
+
+    app.get('/search', (req, res) => {
+        // console.log('res/query: ', req.query); //RES => QUERY:  { q: 'haduc25', ref: 'haduc2509' }
+        console.log('res/query/q: ', req.query.q); //haduc25
+        console.log('res/query/q: ', req.query.ref); //haduc2509
+
+        res.render('search');
+    });
+
+    app.post('/search', (req, res) => {
+        // Với POST thì nhận data qua 'req.body'
+        console.log(req.body); //{ q: 'toiyeubanvclmabandeobienchanvcl', 'full-name': 'bmh <3', age: '18', gender: 'famale'}
+        res.send('SEARCH ĐÃ NHẬN ĐC DATA / METHOD POST');
+    });
+}
+
+module.exports = route;
