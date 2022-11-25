e52dda828fd1c264b571814d87c4ed6ae02e6b12
Ha Duc <haducvcvb@gmail.com>
2022-11-25 16:20:23 +0700

    NodeJS - 34. "Select all" with checkbox / handle forms, sumbit #4

--- NodeJS & ExpressJS/blog/src/resources/views/me/fileForFormatCodeJS.js
+++ NodeJS & ExpressJS/blog/src/resources/views/me/fileForFormatCodeJS.js
@@ -1,7 +1,10 @@
 //Lắng nghe Event khi DOM đc tải xong => call mới đc gọi
 document.addEventListener('DOMContentLoaded', () => {
     var courseId;
+    var containerForm = document.forms['container-form'];
+    //console.log(containerForm);
     var deleteForm = document.forms['delete-course-form'];
+    //console.log(deleteForm);
     var btnDeleteCourse = document.getElementById('btn-delete-course');
     //console.log(btnDeleteCourse);
 
@@ -9,6 +12,9 @@ document.addEventListener('DOMContentLoaded', () => {
     //console.log(checkboxAll);
     var courseItemCheckbox = $('input[name="courseIds[]"]'); //select cả input
     //console.log(courseItemCheckbox)
+
+    var checkAllSubmitBtn = $('.check-all-submit-btn');
+    //console.log(checkAllSubmitBtn)
 
     //When dialog confirm clicked
     $('#delete-course-modal').on('show.bs.modal', function (event) {
@@ -36,21 +42,47 @@ document.addEventListener('DOMContentLoaded', () => {
         //Shorten:
         courseItemCheckbox.prop('checked', isCheckedAll);
         /** // đã rút gọn lại bên trên
-                    if (isCheckedAll) {
-                        courseItemCheckbox.prop('checked', isCheckedAll);
-                    }else{
-                        courseItemCheckbox.prop('checked', isCheckedAll);
-                    }
-            */
+                        if (isCheckedAll) {
+                            courseItemCheckbox.prop('checked', isCheckedAll);
+                        }else{
+                            courseItemCheckbox.prop('checked', isCheckedAll);
+                        }
+                */
+        renderCheckAllSubmitBtn();
     });
 
     // Course item checkbox changed
     courseItemCheckbox.change(function (event) {
         //console.log(123)
-        var lengthOfCheckboxInList = $('input[name="courseIds[]"]:checked').length; //length of checkbox is checked in list courseIds[]
-        var isCheckedAll = courseItemCheckbox.length === lengthOfCheckboxInList; //length of checkbox
+        var lengthOfCheckboxIsChecked = $('input[name="courseIds[]"]:checked').length; //length of checkbox is checked in list courseIds[]
+        var isCheckedAll = courseItemCheckbox.length === lengthOfCheckboxIsChecked; //length of checkbox
         //console.log(isCheckedAll);
 
         checkboxAll.prop('checked', isCheckedAll); // nếu isCheckedAll = fasle => checkboxAll = fasle => ngược lại
+        renderCheckAllSubmitBtn();
     });
+
+    // Check all submit button clicked
+    checkAllSubmitBtn.click(function (event) {
+        //console.log(213);
+        event.preventDefault();
+
+        var isSubmittable = !$(this).hasClass('disabled'); //nếu không có class disabled => true / ngược lại nếu có class disabled => false
+        //console.log(isSubmittable)
+        if (isSubmittable) {
+            containerForm.submit();
+        }
+    });
+
+    // Re-render check all submit button
+    function renderCheckAllSubmitBtn() {
+        var checkedCount = $('input[name="courseIds[]"]:checked').length;
+        //console.log(checkedCount);
+
+        if (checkedCount > 0) {
+            checkAllSubmitBtn.removeClass('disabled');
+        } else {
+            checkAllSubmitBtn.addClass('disabled');
+        }
+    }
 });
