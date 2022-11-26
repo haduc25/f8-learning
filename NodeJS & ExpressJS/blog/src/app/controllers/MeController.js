2547ccc6ea0da99834076f2a31ddeb1660135df6
Ha Duc <haducvcvb@gmail.com>
2022-11-26 14:24:56 +0700

    NodeJS - 37. Sort middleware / K dùng middleware, hasOwnProperty() #2

--- NodeJS & ExpressJS/blog/src/app/controllers/MeController.js
+++ NodeJS & ExpressJS/blog/src/app/controllers/MeController.js
@@ -4,8 +4,37 @@ const { mutipleMongooseToObject } = require('../../util/mongoose');
 class MeController {
     // [GET] /me/stored/courses
     storedCourses(req, res, next) {
+        // Example - k dung middleware
+        let courseQuery = Course.find({});
+
+        if (req.query.hasOwnProperty('_sort')) {
+            // hasOwnProperty(): Kiểm tra '_sort' có tồn tại trong 'req.query' hay không?
+            // res.json({ message: 'Successfully!' });
+            // return;
+            /* 
+                ================> hardcode <================
+            */
+            // courseQuery = courseQuery.sort({
+            //     name: 'asc',
+            //     /** Query.prototype.sort()
+            //      * Sets the sort order
+            //      * If an object is passed, values allowed are asc, desc, ascending, descending, 1, and -1.
+            //      * If a string is passed, it must be a space delimited list of path names. The sort order of each path is ascending unless the path name is prefixed with - which will be treated as descending.
+            //      * Link: https://mongoosejs.com/docs/api/query.html#query_Query-sort
+            //      */
+            // });
+            /* 
+                ================> query - parameter - column, type | req.query.column, req.query.type <================
+            */
+            // res.json(req.query); //{"_sort":"","column":"name","type":"desc"}
+            // return;
+            courseQuery = courseQuery.sort({
+                [req.query.column]: req.query.type,
+            });
+        }
+
         // Gom vào dùng Promise.all
-        Promise.all([Course.find({}), Course.countDocumentsDeleted()])
+        Promise.all([courseQuery, Course.countDocumentsDeleted()])
             .then(([courses, deletedCount]) =>
                 res.render('me/stored-courses', {
                     deletedCount, //truyền qua view/me/stored
