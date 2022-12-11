3e49c70c9a1b76a9d3c09abb17454af573cdf937
Ha Duc <haducvcvb@gmail.com>
2022-12-11 13:26:19 +0700

    Web_App - 8. Import dependencies

--- Web Application/Dont touch your face/reactjs-app/care_your_hand/src/App.js
+++ Web Application/Dont touch your face/reactjs-app/care_your_hand/src/App.js
@@ -1,10 +1,25 @@
+import * as tf from '@tensorflow/tfjs';
+import * as mobilenetModule from '@tensorflow-models/mobilenet';
+import * as knnClassifier from '@tensorflow-models/knn-classifier';
+import { Howl } from 'howler';
+
 import './App.css';
+import onichanSound from './assets/Onii-chan.mp3';
 
 function App() {
+    const classifier = knnClassifier.create();
+    console.log('classifier', classifier);
+
+    var sound = new Howl({
+        src: [onichanSound],
+    });
+
+    sound.play();
+
     return (
         <div className="main">
             <h2>Meow</h2>
-            <video className="video" audioPlay />
+            <video className="video" autoPlay />
 
             <div className="control">
                 <button className="btn">Train 1</button>
