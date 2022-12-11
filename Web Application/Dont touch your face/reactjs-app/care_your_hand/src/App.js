926d73c2a440af4d8d516f1fd4d5996a285adac2
Ha Duc <haducvcvb@gmail.com>
2022-12-11 13:02:29 +0700

    Web_App - 7. Build UI frame

--- Web Application/Dont touch your face/reactjs-app/care_your_hand/src/App.js
+++ Web Application/Dont touch your face/reactjs-app/care_your_hand/src/App.js
@@ -4,6 +4,13 @@ function App() {
     return (
         <div className="main">
             <h2>Meow</h2>
+            <video className="video" audioPlay />
+
+            <div className="control">
+                <button className="btn">Train 1</button>
+                <button className="btn">Train 2</button>
+                <button className="btn">Run</button>
+            </div>
         </div>
     );
 }
