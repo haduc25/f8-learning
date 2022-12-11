31202c9e6b0839c10f300f0ec1924ad8a8c1ddd5
Ha Duc <haducvcvb@gmail.com>
2022-12-11 13:57:36 +0700

    Web_App - 9. Setup camera / event loadeddata, video.current.srcObject

--- Web Application/Dont touch your face/reactjs-app/care_your_hand/src/App.js
+++ Web Application/Dont touch your face/reactjs-app/care_your_hand/src/App.js
@@ -1,3 +1,4 @@
+import { useEffect, useRef } from 'react';
 import * as tf from '@tensorflow/tfjs';
 import * as mobilenetModule from '@tensorflow-models/mobilenet';
 import * as knnClassifier from '@tensorflow-models/knn-classifier';
@@ -7,19 +8,68 @@ import './App.css';
 import onichanSound from './assets/Onii-chan.mp3';
 
 function App() {
-    const classifier = knnClassifier.create();
-    console.log('classifier', classifier);
+    const video = useRef();
 
-    var sound = new Howl({
-        src: [onichanSound],
-    });
+    //// checking classifier working?
+    // const classifier = knnClassifier.create();
+    // console.log('classifier', classifier);
 
-    sound.play();
+    // // Sound
+    // var sound = new Howl({
+    //     src: [onichanSound],
+    // });
+
+    // sound.play();
+
+    // useEffect() | setup Camera
+    const init = async () => {
+        console.log('initing...');
+        await setupCamera();
+
+        console.log('setup camera is success!');
+    };
+
+    // xin cấp quyền truy cập vào camera
+    const setupCamera = () => {
+        return new Promise((resolve, reject) => {
+            // lấy quyền user trong nhiều browser
+            navigator.getUserMedia =
+                navigator.getUserMedia ||
+                navigator.webkitGetUserMedia ||
+                navigator.mozGetUserMedia ||
+                navigator.msGetUserMedia;
+
+            if (navigator.getUserMedia) {
+                navigator.getUserMedia(
+                    { video: true },
+                    (stream) => {
+                        //để set video lên web
+                        video.current.srcObject = stream;
+
+                        // event check success | // nếu load thành công
+                        video.current.addEventListener('loadeddata', resolve);
+                    },
+                    (error) => {
+                        reject(error);
+                    },
+                );
+            } else {
+                reject();
+            }
+        });
+    };
+
+    useEffect(() => {
+        init();
+
+        // cleanup
+        return () => {};
+    }, []);
 
     return (
         <div className="main">
             <h2>Meow</h2>
-            <video className="video" autoPlay />
+            <video ref={video} className="video" autoPlay />
 
             <div className="control">
                 <button className="btn">Train 1</button>
