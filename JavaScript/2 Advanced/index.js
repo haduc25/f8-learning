7edb4dc10e98b56ac654f996aa1ede7b4633d162
Ha Duc <haducvcvb@gmail.com>
2023-09-22 01:35:18 +0700

    JSNC - Tim hieu ve IIFE #1 | 22.9.2023

---
+++ JavaScript/2 Advanced/index.js
@@ -0,0 +1,31 @@
+// // 1. IIFE
+
+const hoTen = 'Minh Duc';
+
+// ((msg, name) => {
+//     console.log('hello: ', msg)
+//     console.log('my name is: ', name)
+// })('Meow', hoTen)
+
+
+//// private
+// let i = 0;
+// (function uwu(){
+//     i++;
+//     console.log(i);
+
+//     if(i < 10)
+//         uwu()
+// })()
+
+
+// các cách tạo ra iife khác
+// // case 1
+// (function () {
+//     console.log('meow 1');
+// }())
+
+// // case 2
+// !function () { // có thể dùng các toán tử phía trước như +, -, !...
+//     console.log('meow 2');
+// }()
