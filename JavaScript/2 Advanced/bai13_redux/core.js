bd64870c6ec35655bea6f623a338c0e2093fcf99
Ha Duc <haducvcvb@gmail.com>
2023-09-29 18:40:32 +0700

    JSNC - Code thư viện build UI Redux_2 #14 | 29.9.2023

--- JavaScript/2 Advanced/bai13_redux/core.js
+++ JavaScript/2 Advanced/bai13_redux/core.js
@@ -24,5 +24,62 @@ export default function html([first, ...strings], ...values){
 export function createStore(reducer) {
     // Nguyên tắc: khi create store thì cần lấy giá trị của `Reducers` vậy nên nhận 1 callback là `reducer`
     // dự liệu trong `store` gọi là `state`
-    let state = reducer() //
+    /** Logic
+     * - khi gọi đến `createStore()` thì truyền `callback` và trong `callback` là `reducer` và sẽ return lại dữ liệu bạn đầu của `store` và lưu vào biến `state`
+     */
+    let state = reducer()
+    const roots = new Map() // chứa root element để render ra view
+    // Map() - có tính chất lặp qua nó, có thể đặt `key` là bất cứ kiểu dl gì trong js
+
+
+    function render(){
+        for (const [root, component] of roots) {
+            /** dùng Destructuring lấy giá trị và lưu vào root & component (thành phần chứa `View`) => laasy dl trong Map()
+             * component: là 1 function sẽ return ra chuỗi html và lưu vào output (const output = component())
+             */
+
+            const output = component() 
+            root.innerHTML = output; //đẩy html vào output => đẩy vào View
+        }
+    }
+
+    return {
+        attach(component, root){
+            // attach: nhận View và đẩy ra `root` ở index.html
+
+            // set lại cho object roots (Map) - sửa dụng Object.set() method để set lại dữ liệu
+            roots.set(root, component) //key là root (là object) & value (là component)
+            render() // sau khi set() xong có dữ liệu thì sẽ render lại ra View
+        },
+        
+        // method kết nối từ `Store` vs `View`
+        connect(selector = state => state){
+            /**
+             * Nhận vào là `selector` & mặc định return lại tất cả `state`
+             * selector: để lựa chọn 1 dữ liệu nào đó cụ thể trong `Store`
+             * selector = state => state: hàm `selector` nhận đối số là 1 biến(state) và sẽ return lại biến đó (state)
+             */
+
+            /** Logic trong hàm `connect`
+             * return lại 1 hàm khác & hàm đó sẽ nhận đối số là `component`
+             * (props, ...args): và trong `component` lại return 1 function trong function đó nhận 1 giá trị là `props` và đối số thứ 2 là toán tử rest `...props` để nhận tất cả args của func
+             * component(): và return và thực thi hàm `component()` | mong muốn khi thực thi đẩy hết dữ liệu của `props`, `args`, `state` vào trong `component`
+             * => vì cả 3 đều là Object nên dùng `Object.assign()` để merge và 1 object mới
+             * selector(state): nhận 1 đối số và return lại đối số đó 
+             */
+            return component => (props, ...args) =>
+                component(Object.assign({}, props, selector(state), ...args))
+        },
+
+        // method giúp `View` có thể thực hiện ` hành động (dispath)
+        dispatch(action, ...args){
+            /** dispatch() là thực hiện hành động nên nhận vào mô tả `action` và đẩy sang cho `reducer` và nhận `...args`
+             *  khi dispatch() được thực thi => gọi lại reducer()
+             */     
+            state = reducer()
+
+
+
+        }
+    }
 }
