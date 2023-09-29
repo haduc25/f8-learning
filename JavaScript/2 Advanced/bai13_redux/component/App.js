c3bb7872018b5d68f7ef3750576117e003dffb25
Ha Duc <haducvcvb@gmail.com>
2023-09-29 23:59:24 +0700

    JSNC - Code thư viện build UI Redux_5 #17 | 29.9.2023

--- JavaScript/2 Advanced/bai13_redux/component/App.js
+++ JavaScript/2 Advanced/bai13_redux/component/App.js
@@ -3,27 +3,57 @@ import { connect } from "../store.js";
 
 const connector = connect() 
 
+/*// Example1: - select mỗi `cars` trong object `init`
+const connector = connect(state => ({
+    cars: state.cars
+}))
+*/
+
+/*
+// Example2: - đẩy dữ liệu để lưu vào 1 key mới
+const connector = connect(state => ({
+    car: state.cars[0],
+    cars: state.cars
+    // {car: 'Porsche 911', cars: Array(4)}
+}))
+*/
+
+/** const connector = connect() 
+ *  Nhận lại `state` và return ra 1 object mới có `cars` = state.cars
+ * 
+*/
+
 /** const connector = connect() 
  * chạy hàm `connect()` để return về hàm bên trong và nhận đối số là `component`
  * và khi chạy xong nhận lại hàm `connector` là 1 hàm mới
  * hàm `connector` sẽ nhận đối số là `component` => cho nhận luôn là App => export default connector(App)
  */ 
 
-const App = (props) => {
+// const App = (props) => {
+const App = ({cars, bike}) => {
     /** Mặc định sẽ nhận được dữ liệu là `props`
      * vì `connector` nhận vào `component` xong thực thi `component()` và đẩy dữ liệu từ `state` ngược vào trong `component` (compnent hiện tại đang là App) 
      * và `props` nhận được hết dữ liệu đã được đẩy vào (là dl trong bên core.js & connect() là props, state, ...args)
+     * => thay vào đó dùng Destructuring để lấy ra `cars`
      */
 
-    console.log(props) // {cars: Array(4)}
+    // console.log(props) // {cars: Array(4)}
+    // console.log(bike) //['bike1']
 
 
     return html`
-        <h2>I loved Porsche 781</h2>
+        <h2>Luv You</h2>
         <ul>
-
+            ${cars.map((car)=> `<li>${car}</li>`) }
         </ul>
-    `
+
+        <button onclick="dispatch('ADD', 'Audi')">Add car</button>
+        `
+        /**  <button onclick="dispatch()">Add car</button>
+         * gọi đến function `dispatch` vì trong `store.js` đã tạo ra 1 biến `dispatch` là global
+         * `dispatch` nhận `action`, `args`
+         * dispatch('ADD', 'Audi')
+        */
 }
 
 
--- JavaScript/2 Advanced/bai13_redux/reducer.js
+++ JavaScript/2 Advanced/bai13_redux/reducer.js
@@ -1,5 +1,6 @@
 const init = {
-    cars: ['Porsche 911', 'Porsche 781', 'Mazda', 'KIA K5']
+    cars: ['Porsche 911', 'Porsche 781', 'Mazda', 'KIA K5'],
+    bike: ['bike1']
 }
 
 export default function reducer(state = init, action, args){
@@ -9,7 +10,20 @@ export default function reducer(state = init, action, args){
      *  let state = reducer(): lần đầu tiên chạy k có đối số gì => return về `state` chính là `init` (và đang chứa obj cars)
      *  dispatch(action, ...args){ state = reducer(state, action, args)}: đến khi được gọi mới sẽ truyền `action` vào từ switch case dưới (vd như add, create, delete...)
      */
+
+    console.log(action, args) // ADD ['Audi']
+    
     switch(action){
+        case 'ADD':
+            // Audi đang nằm trong mảng của `args` nên lấy ra
+            const [newCar] = args
+            return {
+                ...state, // giải date từ `state` cũ vào object mới 
+                cars: [...state.cars, newCar]  // giải object cũ của `cars` & thêm car mới (newCar) vào cuối mảng
+            }
+
+            break;
+
         default:
             return state
     }
