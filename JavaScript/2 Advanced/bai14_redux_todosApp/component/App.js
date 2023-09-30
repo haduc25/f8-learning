4bab93205ab8ec082ac4803cd3fb6036d051a339
Ha Duc <haducvcvb@gmail.com>
2023-09-30 17:59:52 +0700

    JSNC - Redux__todosApp_1 #20 | 30.9.2023

---
+++ JavaScript/2 Advanced/bai14_redux_todosApp/component/App.js
@@ -0,0 +1,10 @@
+import html from '../core.js'
+
+
+const App = () => {
+    return html`
+        <h2>I LOVED MON3Y</h2>
+    `
+}
+
+export default App
---
+++ JavaScript/2 Advanced/bai14_redux_todosApp/reducer.js
@@ -0,0 +1,11 @@
+const init = {
+    
+}
+
+
+export default function reducer(state = init, action, args){
+    switch(action){
+        default:
+            return state
+    }
+}
